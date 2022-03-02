21aa3a5bc802897f2ef1698a33d94f5ed5b1856e
Jakub Kusz <259267@student.pwr.edu.pl>
2022-03-02 10:36:50 +0100

    adding cmakefile

---
+++ build/CMakeFiles/tic_tac_toe.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kubus/Pulpit/tic_tac_toe/src/imgui.cpp" "CMakeFiles/tic_tac_toe.dir/src/imgui.cpp.o" "gcc" "CMakeFiles/tic_tac_toe.dir/src/imgui.cpp.o.d"
+  "/home/kubus/Pulpit/tic_tac_toe/src/imgui_demo.cpp" "CMakeFiles/tic_tac_toe.dir/src/imgui_demo.cpp.o" "gcc" "CMakeFiles/tic_tac_toe.dir/src/imgui_demo.cpp.o.d"
+  "/home/kubus/Pulpit/tic_tac_toe/src/imgui_draw.cpp" "CMakeFiles/tic_tac_toe.dir/src/imgui_draw.cpp.o" "gcc" "CMakeFiles/tic_tac_toe.dir/src/imgui_draw.cpp.o.d"
+  "/home/kubus/Pulpit/tic_tac_toe/src/imgui_impl_glfw.cpp" "CMakeFiles/tic_tac_toe.dir/src/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/tic_tac_toe.dir/src/imgui_impl_glfw.cpp.o.d"
+  "/home/kubus/Pulpit/tic_tac_toe/src/imgui_impl_opengl3.cpp" "CMakeFiles/tic_tac_toe.dir/src/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/tic_tac_toe.dir/src/imgui_impl_opengl3.cpp.o.d"
+  "/home/kubus/Pulpit/tic_tac_toe/src/imgui_tables.cpp" "CMakeFiles/tic_tac_toe.dir/src/imgui_tables.cpp.o" "gcc" "CMakeFiles/tic_tac_toe.dir/src/imgui_tables.cpp.o.d"
+  "/home/kubus/Pulpit/tic_tac_toe/src/imgui_widgets.cpp" "CMakeFiles/tic_tac_toe.dir/src/imgui_widgets.cpp.o" "gcc" "CMakeFiles/tic_tac_toe.dir/src/imgui_widgets.cpp.o.d"
+  "/home/kubus/Pulpit/tic_tac_toe/src/main.cpp" "CMakeFiles/tic_tac_toe.dir/src/main.cpp.o" "gcc" "CMakeFiles/tic_tac_toe.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/tic_tac_toe.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tic_tac_toe.dir/src/imgui.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/src/imgui.cpp.o.d"
+  "CMakeFiles/tic_tac_toe.dir/src/imgui_demo.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/src/imgui_demo.cpp.o.d"
+  "CMakeFiles/tic_tac_toe.dir/src/imgui_draw.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/src/imgui_draw.cpp.o.d"
+  "CMakeFiles/tic_tac_toe.dir/src/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/src/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/tic_tac_toe.dir/src/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/src/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/tic_tac_toe.dir/src/imgui_tables.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/src/imgui_tables.cpp.o.d"
+  "CMakeFiles/tic_tac_toe.dir/src/imgui_widgets.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/src/imgui_widgets.cpp.o.d"
+  "CMakeFiles/tic_tac_toe.dir/src/main.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/src/main.cpp.o.d"
+  "tic_tac_toe"
+  "tic_tac_toe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tic_tac_toe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
