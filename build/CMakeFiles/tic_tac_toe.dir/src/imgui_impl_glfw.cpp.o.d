c572de34eea700b8754fb55cd0fa9a767263dbef
Jakub Kusz <259267@student.pwr.edu.pl>
2022-03-02 10:36:50 +0100

    launching_cmake

---
+++ build/CMakeFiles/tic_tac_toe.dir/src/imgui_impl_glfw.cpp.o.d
@@ -0,0 +1,27 @@
+CMakeFiles/tic_tac_toe.dir/src/imgui_impl_glfw.cpp.o: \
+ /home/kubus/Pulpit/tic_tac_toe/src/imgui_impl_glfw.cpp \
+ /usr/include/stdc-predef.h /home/kubus/Pulpit/tic_tac_toe/inc/imgui.h \
+ /home/kubus/Pulpit/tic_tac_toe/inc/imconfig.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/float.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdarg.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stddef.h /usr/include/string.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+ /usr/include/strings.h /usr/include/assert.h \
+ /home/kubus/Pulpit/tic_tac_toe/inc/imgui_impl_glfw.h \
+ /home/kubus/Pulpit/imgui/imgui/examples/libs/glfw/include/GLFW/glfw3.h \
+ /usr/lib/gcc/x86_64-linux-gnu/9/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h /usr/include/GL/gl.h \
+ /usr/include/GL/glext.h /usr/include/KHR/khrplatform.h
